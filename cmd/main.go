5d46260cbde4a1a57b1d104287712b4e41451db4
10Narratives <vanyatertyshnikov@yandex.ru>
2025-01-09 19:29:29 +0300

    Add database connection and handlers in main

--- cmd/main.go
+++ cmd/main.go
@@ -4,17 +4,26 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/10Narratives/todo-list/database"
+	"github.com/10Narratives/todo-list/handlers"
 	"github.com/go-chi/chi/v5"
 )
 
 func main() {
-	r := chi.NewRouter()
+	database.ConnectDB()
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+	router := chi.NewRouter()
+
+	router.Get("/tasks", handlers.GetTasks)
+	router.Post("/tasks", handlers.PostTask)
+	router.Get("/tasks/{id}", handlers.GetTaskByID)
+	router.Delete("/tasks/{id}", handlers.DeleteTask)
+
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Привет, программист!"))
 	})
 
-	if err := http.ListenAndServe(":3000", r); err != nil {
+	if err := http.ListenAndServe(":3000", router); err != nil {
 		fmt.Printf("Start server error: %s", err.Error())
 		return
 	}
