c7ee86a2ffd434b2a90731e7992419350cde5ad1
10Narratives <vanyatertyshnikov@yandex.ru>
2025-01-09 19:23:17 +0300

    Add docs for Task struct

--- models/models.go
+++ models/models.go
@@ -2,6 +2,13 @@ package models
 
 import "gorm.io/gorm"
 
+// Task represents a task entity in the application.
+//
+// Fields:
+//
+// - ID: A unique identifier for the task, automatically managed by GORM.
+// - Description: A brief description of the task. This field is stored as text.
+// - Note: Additional notes related to the task, also stored as text.
 type Task struct {
 	gorm.Model
 	ID          uint   `json:"id" gorm:"primaryKey"`
